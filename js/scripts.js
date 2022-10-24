df7687db356ae6ab0754b5038ad00c1d8f7a0341
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-10-24 12:04:00 +0300

    create newString(string) and window.prompt() for a sentence

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//business logic
+function newString(string)  {
+  let firstLetter = string.slice(0, 1);
+  firstLetter = firstLetter.toUpperCase();
+  window.alert(firstLetter);
+  let lastLetter = string.slice(-1);
+  lastLetter = lastLetter.toUpperCase();
+  window.alert(lastLetter);
+}
+//user interface
+let  string = window.prompt("Please enter a sentence");
+window.alert(string);
+newString(string);
