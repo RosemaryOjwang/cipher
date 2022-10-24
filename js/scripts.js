9a0f5e685f5015dec495b72f0158e5cb8a1b9739
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-10-24 14:31:57 +0300

    amend function calls in reverse(string, call() and countLetter(string) functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,45 +2,40 @@
 let newStr;
 let reversedString;
 let finalString;
-function newString(string)  {
+
+function newString(string) {
   let firstLetter = string.slice(0, 1);
   firstLetter = firstLetter.toUpperCase();
-  window.alert(firstLetter);
   let lastLetter = string.slice(-1);
   lastLetter = lastLetter.toUpperCase();
-  window.alert(lastLetter);
   newStr = firstLetter + lastLetter;
-  window.alert(newStr);
   return newStr;
 }
-function reverse(string)  {
-  let firstLetter = newStr.slice(0,1);
-  let secondLetter = newStr.slice(-1);
-  reversedString = secondLetter.concat(firstLetter);
-  window.alert(reversedString);
+
+function reverse(string) {
+  reversedString = newString(string).split("").reverse().join("");
   return reversedString;
 }
+
 function call() {
-  newString(string);
-  reverse(newStr);
-  finalString = string.concat(reversedString);
-  window.alert(finalString);
+  let twoLetterStr = newString(string);
+  reverse(twoLetterStr);
+  finalString = string.concat(reverse(twoLetterStr));
   return finalString;
 }
+
 function countLetter(string) {
-let lengthOfString = finalString.length;
-window.alert(lengthOfString);
-let letter =finalString.charAt(Math.floor(lengthOfString / 2));
-window.alert(letter);
-let newSentence = letter.concat(finalString)
-window.alert(newSentence);
-let reversedSentence =newSentence.split("").reverse().join("");
-window.alert(reversedSentence);
+  let lengthOfString = string.length;
+  window.alert(lengthOfString);
+  let letter = call().charAt(Math.floor(lengthOfString / 2));
+  let newSentence = letter.concat(call());
+  let reversedSentence = newSentence.split("").reverse().join("");
+  window.alert(reversedSentence);
 
 
 }
 //user interface
-let  string = window.prompt("Please enter a sentence");
+let string = window.prompt("Please enter a sentence");
 window.alert(string);
 call();
 countLetter(string);
